cf20b83ff4c957fb27449431f61fa8ece3d0780e
Jheyvie Santos <134067501+Jheyvie-Santos@users.noreply.github.com>
2023-08-07 18:37:10 +0800

    Intl commit

---
+++ Reusable-sidenav/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // Function to load content based on pageId
+    function loadContent(pageId) {
+        const filename = pageId + '/' + pageId + '.html';
+        const cssFile = 'pages/' + pageId + '/' + pageId + '.css';
+        const jsFile = 'pages/' + pageId + '/' + pageId + '.js';
+        const filePath = 'pages/' + filename;
+
+        $.get(filePath, function(data) {
+            $('.content').html(data);
+
+            // Load CSS content
+            $.get(cssFile, function(cssData) {
+                $('<style>').html(cssData).appendTo('head');
+            });
+
+            // Load JavaScript file
+            $.getScript(jsFile);
+        });
+    }
+
+    // Load default content ('dashboard.html')
+    loadContent('dashboard');
+
+    // Click event listener for navigation
+    $('.navigations ul li').click(function() {
+        const pageId = $(this).attr('id');
+        loadContent(pageId);
+    });
+});
